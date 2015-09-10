38f6af931328333cd8a4ee8e7c0d357c0394da07
Wai Hwa Tan <twaih92@gmail.com>
2015-09-10 12:31:44 -0700

    moved Sandal to own file. export works

--- sandal.js
+++ sandal.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import Sequelize from 'sequelize';
+import {graphql} from 'graphql';
 
 let app = express();
 
-app.use(bodyParser.urlencoded());
+console.log('Sandal is running');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 async function graphQLHandler(req, res, schema){
+  console.log('in graphqlhandler');
+  console.log('req ',req);
+  console.log('req.body', req.body);
   const {query, variables = {}} = req.body;
   console.log(query);
   console.log(variables);
@@ -20,6 +28,7 @@ async function graphQLHandler(req, res, schema){
 
 function Sandal(schema,uri){//query, mutation, uri) {
   var sequelize = new Sequelize(uri);
+  console.log('Sandal init');
 
   let User = sequelize.define('users', {
     name: {
@@ -38,7 +47,8 @@ function Sandal(schema,uri){//query, mutation, uri) {
   // get rid of this
   sequelize.sync();
 
-  console.log(schema);//._schemaConfig.query._fields);
+  //console.log(schema);//._schemaConfig.query._fields);
+  //console.log(schema._schemaConfig.query._fields);
   schema._schemaConfig.query._fields.getUser.resolve = (root, {name})=>{
     console.log('resolving in getUser');
     console.log('root in getUser: ',root);
@@ -54,6 +64,10 @@ function Sandal(schema,uri){//query, mutation, uri) {
   }
   console.log(schema._schemaConfig.query._fields);
   return function(req, res) {
+    console.log('pre graphqlhandler');
+    console.log('req body in Callback ',req.body);
     graphQLHandler(req, res, schema);
   }
 }
+
+module.exports = Sandal;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import {graphql} from 'graphql';
 import Schema from './data/schema.js';
 import bodyParser from 'body-parser';
 import Sequelize from 'sequelize';
+var Sandal = require('./sandal.js');
 
 
 let app = express();
@@ -10,71 +11,12 @@ let app = express();
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded());
 
-async function graphQLHandler(req, res, schema){
-  const {query, variables = {}} = req.body;
-  console.log(query);
-  console.log(variables);
-  const result = await graphql(
-    schema,
-    query,
-    {},
-    variables
-  );
-  res.send(result);
-}
-
-// app.post('/', graphQLHandler);
-
-// var Sandal = require('sandle');
 var cb = Sandal(Schema, 'postgres://localhost/test'); // => function(req, res) { }
+
 app.post('/',cb);
-
-
-
-function Sandal(schema,uri){//query, mutation, uri) {
-  var sequelize = new Sequelize(uri);
-
-  let User = sequelize.define('users', {
-    name: {
-      type: Sequelize.STRING,
-      field: 'name'
-    },
-    age: {
-      type: Sequelize.INTEGER,
-      field: 'age'
-    },
-  });
-
-  // get rid of this
-  User.belongsToMany(User, {as: 'friends', through: 'friendships'});
-
-  // get rid of this
-  sequelize.sync();
-
-  console.log(schema);//._schemaConfig.query._fields);
-  schema._schemaConfig.query._fields.getUser.resolve = (root, {name})=>{
-    console.log('resolving in getUser');
-    console.log('root in getUser: ',root);
-    console.log('name arg in getUser: ',name);
-    //return User
-    console.log('User in getUser: ', User);
-    console.log('got here!');
-    return User
-      .findOne({
-        where: { name : name }
-      })
-    console.log('getUser returned: ',user);
-  }
-  console.log(schema._schemaConfig.query._fields);
-  return function(req, res) {
-    graphQLHandler(req, res, schema);
-  }
-}
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Server is listening on port 3000.");
-  //sequelize = new Sequelize('postgres://localhost/test');
 });
 
 module.exports = app;
-//module.exports.sequelize = sequelize;
