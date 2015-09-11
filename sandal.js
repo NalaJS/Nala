12f30a05e7878ddef9e21818ec443d21a86501c8
Wai Hwa Tan <twaih92@gmail.com>
2015-09-11 12:32:07 -0700

    workin on converting GraphQLSchema to Sequelize Model

--- sandal.js
+++ sandal.js
@@ -54,11 +54,13 @@ function Sandal(schema,uri){//query, mutation, uri) {
   //   console.log('Key is: ', key);
   // }
   //TODO: find better name
+  var defaultNames = ['String', 'query', 'Int', 'mutation', 'Boolean'];
   var GraphQLModels = Object.keys(schema._typeMap).filter(function(elem) {
-    return elem !== 'String' && elem !== 'query' && elem !== 'Int' && elem !== 'mutation' && elem !== 'Boolean' && (elem[0] !== '_' || elem[1] !== '_');
+    return defaultNames.indexOf(elem) < 0 && (elem[0] !== '_' || elem[1] !== '_');
   });
 
-  console.log(schema._typeMap[GraphQLModels[0]]);
+console.log("model fields name type...  ", schema._typeMap[GraphQLModels[0]]._fields.age.type);
+  // console.log(schema._typeMap[GraphQLModels[0]]);
   // { name: 'user',
   //   description: 'this is the user type',
   //   isTypeOf: undefined,
@@ -79,6 +81,53 @@ function Sandal(schema,uri){//query, mutation, uri) {
   }
 }
 
+//this function takes in the user created schema in GraphQLSchema object
+//and converts it into Sequelize model definitions and subsequently runs them.
+
+function convertSchema(modelNames, typeMap){
+  var sequelizeArr = [];
+  for(var i = 0; i < modelNames.length; i++) {
+    var model = typeMap[modelNames[i]],
+        fields = Object.keys(model._fields), //['name', 'age']
+        sequelizeSchema = {},
+        sequelizeFieldTypes = {String: Sequelize.STRING, Int: Sequelize.INTEGER};
+    for(var j = 0; j < fields.length; j++) {
+      sequelizeSchema[fields[j]] = {
+        type: sequelizeFieldTypes[model._fields.fields[j].type.name],
+        field: fields[j]
+      };
+    }
+    sequelizeArr.push(sequelize.define(modelNames[i], sequelizeSchema));
+  }
+    return sequelizeArr;
+  //
+  //to be created:
+  // let User = sequelize.define('users', {
+  //   name: {
+  //     type: Sequelize.STRING,
+  //     field: 'name'
+  //   },
+  //   age: {
+  //     type: Sequelize.INTEGER,
+  //     field: 'age'
+  //   },
+  // });
+
+  //we take in this...
+  // { name: 'user',
+  //   description: 'this is the user type',
+  //   isTypeOf: undefined,
+  //   _typeConfig:
+  //    { name: 'user',
+  //      description: 'this is the user type',
+  //      fields: { name: [Object], age: [Object] } },
+  //   _interfaces: [],
+  //   _fields:
+  //    { name: { type: [Object], name: 'name', args: [] },
+  //      age: { type: [Object], name: 'age', args: [] } } }
+
+
+}
 // for(let i = 0; i < GraphQLModels.length; i++){
 //
 // }
