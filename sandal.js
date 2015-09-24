cc7c6d8fd3b05080d379885b88fbdb2928fad804
Wai Hwa Tan <twaih92@gmail.com>
2015-09-23 17:27:40 -0700

    moved relation creators and removers to functions outside of initSequelizeRelations

--- sandal.js
+++ sandal.js
@@ -163,85 +163,92 @@ function initSequelizeRelations(relations, typeMap, mutationFields){
           })
     }
 
-    //TODO: Modularize!
-    //creates relations creators
-    // console.log('getterName,',getterName);
-    // console.log('creatorName,',creatorName); //addFriends
-    // console.log('typemap inputval', typeMap.__InputValue._fields);
+    createRelationCreators(creatorName, relationName, tables, table1Name, table2Name, mutationFields, typeMap)
 
-    mutationFields[creatorName] = {
-      type: typeMap.String //success/error
-    };
-
-    mutationFields[creatorName].args = [{
-      name: 'model1',
-      type: typeMap.String,
-      description: null,
-      defaultValue: null
-    },
-    {
-      name: 'model2',
-      type: typeMap.String,//[modelFields[field].type.name],
-      description: null,
-      defaultValue: null
-    }];
-
-    //
-    mutationFields[creatorName].resolve = (root, {model1, model2})=>{
-        // console.log('in resolve of',creatorName);
-        var m1 = JSON.parse(model1);
-        var m2 = JSON.parse(model2);
-        // console.log('add'+relationName.charAt(0).toUpperCase()+relationName.slice(1,relationName.length-1));
-            tables[table1Name].findOne({
-                where: m1//{name: model1}
-              }).then(function(found1, created){
-                tables[table2Name].findOne({
-                  where: m2//{name: model2}
-                }).then(function(found2, created){
-                  //TODO: currently hacky way of turning addFriends -> addFriend, which is created by Sequelize
-                  found1['add'+relationName.charAt(0).toUpperCase()+relationName.slice(1,relationName.length-1)](found2);
-                })
-              });
-          }
-
-    //TODO: Modularize!
-    //create relationRemovers
-    //console.log('destroyerName,',destroyerName); //removeFriends
-
-    mutationFields[destroyerName] = {
-      type: typeMap.String //success/error
-    };
-
-    mutationFields[destroyerName].args = [{
-      name: 'model1',
-      type: typeMap.String,
-      description: null,
-      defaultValue: null
-    },
-    {
-      name: 'model2',
-      type: typeMap.String,//[modelFields[field].type.name],
-      description: null,
-      defaultValue: null
-    }];
-
-    mutationFields[destroyerName].resolve = (root, {model1, model2})=>{
-        var m1 = JSON.parse(model1);
-        var m2 = JSON.parse(model2);
-        // console.log('remove'+relationName.charAt(0).toUpperCase()+relationName.slice(1,relationName.length-1));
-            tables[table1Name].findOne({
-                where: m1
-              }).then(function(found1, created){
-                tables[table2Name].findOne({
-                  where: m2
-                }).then(function(found2, created){
-                  //TODO: currently hacky way of turning addFriends -> addFriend, which is created by Sequelize
-                  found1['remove'+relationName.charAt(0).toUpperCase()+relationName.slice(1,relationName.length-1)](found2);
-                })
-              });
-          }
-
+    createRelationRemovers(destroyerName, relationName, tables, table1Name, table2Name, mutationFields, typeMap)
   }
+}
+
+/*
+Creator
+params:
+mutationFields
+creatorName
+typeMap
+tables
+table1Name
+table2Name
+relationName
+*/
+function createRelationCreators(creatorName, relationName, tables, table1Name, table2Name, mutationFields, typeMap){
+  mutationFields[creatorName] = {
+    type: typeMap.String //success/error
+  };
+
+  mutationFields[creatorName].args = [{
+    name: 'model1',
+    type: typeMap.String,
+    description: null,
+    defaultValue: null
+  },
+  {
+    name: 'model2',
+    type: typeMap.String,//[modelFields[field].type.name],
+    description: null,
+    defaultValue: null
+  }];
+
+  mutationFields[creatorName].resolve = (root, {model1, model2})=>{
+      // console.log('in resolve of',creatorName);
+      var m1 = JSON.parse(model1);
+      var m2 = JSON.parse(model2);
+      // console.log('add'+relationName.charAt(0).toUpperCase()+relationName.slice(1,relationName.length-1));
+          tables[table1Name].findOne({
+              where: m1//{name: model1}
+            }).then(function(found1, created){
+              tables[table2Name].findOne({
+                where: m2//{name: model2}
+              }).then(function(found2, created){
+                //TODO: currently hacky way of turning addFriends -> addFriend, which is created by Sequelize
+                found1['add'+relationName.charAt(0).toUpperCase()+relationName.slice(1,relationName.length-1)](found2);
+              })
+            });
+        }
+}
+
+function createRelationRemovers(destroyerName, relationName, tables, table1Name, table2Name, mutationFields, typeMap){
+  mutationFields[destroyerName] = {
+    type: typeMap.String //success/error
+  };
+
+  mutationFields[destroyerName].args = [{
+    name: 'model1',
+    type: typeMap.String,
+    description: null,
+    defaultValue: null
+  },
+  {
+    name: 'model2',
+    type: typeMap.String,//[modelFields[field].type.name],
+    description: null,
+    defaultValue: null
+  }];
+
+  mutationFields[destroyerName].resolve = (root, {model1, model2})=>{
+      var m1 = JSON.parse(model1);
+      var m2 = JSON.parse(model2);
+      // console.log('remove'+relationName.charAt(0).toUpperCase()+relationName.slice(1,relationName.length-1));
+          tables[table1Name].findOne({
+              where: m1
+            }).then(function(found1, created){
+              tables[table2Name].findOne({
+                where: m2
+              }).then(function(found2, created){
+                //TODO: currently hacky way of turning addFriends -> addFriend, which is created by Sequelize
+                found1['remove'+relationName.charAt(0).toUpperCase()+relationName.slice(1,relationName.length-1)](found2);
+              })
+            });
+        }
 }
 
 module.exports = Sandal;
