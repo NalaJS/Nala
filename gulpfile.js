b84dffc470feef789f0bbedba19ff7717e685848
Wai Hwa Tan <twaih92@gmail.com>
2015-10-19 19:35:37 -0700

    updated gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,35 +2,49 @@ var gulp = require('gulp'),
 browserify = require('browserify'),
 reactify = require('reactify'),
 watchify = require('watchify'),
-source = require('vinyl-source-stream');
+source = require('vinyl-source-stream'),
+notify = require('gulp-notify');
 
-gulp.task('browserify', scripts);
+function handleErrors() {
+	var args = Array.prototype.slice.call(arguments);
+	notify.onError({
+		title: 'Compile Error',
+		message : '<%= error.message %>'
+	}).apply(this, args);
 
-function scripts(){
-	var bundler = browserify('./client/main.js');
+	this.emit('end'); // keeps gulp from hanging on this task
+}
 
-	var watcher = watchify(bundler);
+function buildScript(file, watch) {
+	var props = {
+		entries : ['./client/' + file],
+		debug : true,
+		transform : [reactify]
+	}
 
-	return watcher
-		.on('update', function(){
-			var updateStart = Date.now();
-			watcher
-			.transform(reactify)
-			.bundle()
-			.on('error', function(err){
-				console.log('Error compiling components', err.message);
-			})
+	var bundler = watch ? watchify(browserify(props)) : browserify(props);
+
+	function rebundle() {
+		var stream = bundler.bundle();
+		return stream
+			.on('error', handleErrors)
 			.pipe(source('bundle.js'))
-			.pipe(gulp.dest('./client/build'));
-			console.log('Updated!', (Date.now() - updateStart) + 'ms');
-		})
-		.transform(reactify)
-		.bundle()
-		.on('error', function(err){
-			console.log('Error compiling components', err.message);
-		})
-		.pipe(source('bundle.js'))
-		.pipe(gulp.dest('./client/build/'));
-};
+			.pipe(gulp.dest('./client/build/'))
+	}
 
-gulp.task('default', ['browserify']);
+	bundler.on('update', function() {
+		var updateStart = Date.now();
+		rebundle();
+		console.log('Updated!', (Date.now() - updateStart) + "ms");
+	});
+
+	return rebundle();
+}
+
+gulp.task('scripts', function() {
+	return buildScript('main.js', false);
+});
+
+gulp.task('default', ['scripts'], function() {
+	return buildScript('main.js', true);
+});
